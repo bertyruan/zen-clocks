ad089876d4acf437a4d41dab0d35d573fc0e48ad
Albert Ruan <berty.ruan@gmail.com>
2021-10-23 14:23:24 +0800

    adds profile files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SandboxComponent } from './sandbox/sandbox.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TetrisComponent } from './tetris/tetris.component';
-import { HomeComponent } from './home/home.component';
 import { ZenclocksComponent } from './zenclocks/zenclocks.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "timer", pathMatch: 'full'},
-  {path: "sandbox", component: SandboxComponent},
   {path: "tetris", component: TetrisComponent},
   {path: "timer", component: ZenclocksComponent}
   //{path: "stopwatch", loadChildren: () => import('./stopwatch/stopwatch.module').then(m => m.StopwatchModule),}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-import { SandboxComponent } from './sandbox/sandbox.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TetrisComponent } from './tetris/tetris.component';
-import { HomeComponent } from './home/home.component';
 import { ZenclocksModule } from './zenclocks/zenclocks.module';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -14,10 +11,7 @@ import { CommonModule } from '@angular/common';
 
 @NgModule({ 
   declarations: [
-    AppComponent,
-    SandboxComponent,
-    TetrisComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
