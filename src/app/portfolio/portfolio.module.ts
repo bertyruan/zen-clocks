09104b58b81c87732b505d2f3c46971234e222de
Albert Ruan <berty.ruan@gmail.com>
2021-10-23 21:13:19 +0800

    adds nav bar

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -7,6 +7,13 @@ import { PortfolioRoutingModule } from "./portfolio-routing.module";
 import { PortfolioComponent } from "./portfolio.component";
 import { WorkComponent } from "./work/work.component";
 import { MatSliderModule } from '@angular/material/slider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
     declarations: [
@@ -19,7 +26,14 @@ import { MatSliderModule } from '@angular/material/slider';
         PortfolioRoutingModule,
         CommonModule,
         FlexLayoutModule,
-        MatSliderModule 
+        MatSliderModule,
+        MatGridListModule,
+        MatCardModule,
+        MatMenuModule,
+        MatIconModule,
+        MatButtonModule,
+        LayoutModule,
+        MatToolbarModule
     ],
     bootstrap: []
 })
