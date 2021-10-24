685bab68b6bfd0119b36d92db4917514ddf3f275
Albert Ruan <berty.ruan@gmail.com>
2021-10-24 11:44:18 +0800

    finishes work

--- src/app/portfolio/work/work.component.ts
+++ src/app/portfolio/work/work.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-work',
@@ -8,26 +9,35 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./work.component.scss']
 })
 export class WorkComponent {
+  works = [
+    {
+      title: "Zen Clocks",
+      description: `
+        The user can create multiple timers. 
+        When one ends, the other starts. Sessions can be saved and restored. 
+        Built on HTML, CSS, Angular and RxJS.
+        `
+    }
+  ]
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { isVisible: true, work: this.works[0], cols: 3, rows: 1 }
         ];
       }
-
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { isVisible: false, cols: 1, rows: 1},
+        { isVisible: true, work: this.works[0], cols: 1, rows: 1 }
+  
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private route: Router) {}
+
+  navigateTo(event: any) : void {
+    this.route.navigate([]).then(result => { window.open("./zen-clocks", '_blank')})
+  }
 }
