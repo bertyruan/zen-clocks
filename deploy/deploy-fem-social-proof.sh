f2ed9e1cf5173ae5daacc8dc2f6ab65ef68d19d1
Albert Ruan <berty.ruan@gmail.com>
2022-02-16 16:59:39 +0800

    update deploy script

--- deploy/deploy-fem-social-proof.sh
+++ deploy/deploy-fem-social-proof.sh
@@ -3,4 +3,7 @@
 cd ~/berty/fem-social-proof
 git pull
 rm -vf ~/htdocs/portfolio/fem-social-proof/*
-cp -vf * ~/htdocs/portfolio/fem-social-proof
+cp -vf index.html main.css default.css ~/htdocs/portfolio/fem-social-proof
+
+# cp -vfr * ~/htdocs/portfolio/fem-social-proof
+# rm -vfr ~/htdocs/portfolio/fem-social-proof/*
